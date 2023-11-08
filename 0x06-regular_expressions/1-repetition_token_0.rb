559b71a5dca2c46d2b4fc8e6713d7af53ba78c62
siphokazi <usphow@gmail.com>
2023-11-08 08:58:15 -0800

    Textme

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,29 +1,3 @@
 #!/usr/bin/env ruby
+puts ARGV[0].scan(/hbt{2,5}n/).join
 
-# Import the Oniguruma library
-require 'oniguruma'
-
-# Define a method that accepts a string and a pattern
-def match_pattern(string, pattern)
-  # Create a new regular expression with the pattern
-  regex = Oniguruma::ORegexp.new(pattern)
-
-  # Use the match method to check if the string matches the pattern
-  match_data = regex.match(string)
-
-  # If there's a match, print the match. Otherwise, print 'No match'.
-  if match_data
-    puts "Matched: #{match_data[0]}"
-  else
-    puts 'No match'
-  end
-end
-
-# Get the string from the command line argument
-string = ARGV[0]
-
-# Define the pattern
-pattern = 'hb(t*)n'
-
-# Call the method with the string and the pattern
-match_pattern(string, pattern)
