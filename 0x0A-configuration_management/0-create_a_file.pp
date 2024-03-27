250c79f021a257af797b2c85ffe932556da03d00
siphokazi <usphow@gmail.com>
2024-03-27 14:45:31 +0000

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Creates a file in /tmp
 
 file { '/tmp/shcool':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+	content	=>'I love Puppet',
+	mode	=> '0744',
+	owner	=> 'www-data',
+	group	=> 'www-data',
 }
